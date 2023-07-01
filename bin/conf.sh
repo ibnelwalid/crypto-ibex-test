e5f11393c183499f70f33009ffb1af17ed248ff6
Yahia Khalid <y.k.ibnelwalid@gmail.com>
2023-07-01 19:02:18 +0200

    Change ibex repo path change submodule to point for zk_ext branch

--- bin/conf.sh
+++ bin/conf.sh
@@ -7,6 +7,6 @@ export REPO_VERSION_PATCH="0"
 export REPO_VERSION="${REPO_VERSION_MAJOR}.${REPO_VERSION_MINOR}.${REPO_VERSION_PATCH}"
 
 export RISCV=${REPO_HOME}/build/riscv
-export IBEX_REPO=${REPO_HOME}/build/ibex
+export IBEX_REPO=${REPO_HOME}/src/hw/ibex
 
 
